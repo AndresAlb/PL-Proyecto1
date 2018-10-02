203d46784720035e2f8e283bf35b04af2762f14d
Andres Albores <aangele6@itam.mx>
2018-10-02 11:41:36 -0500

    Primera version Metodo Simplex revisado
    mSimplexFaseII contiene la 1ra versión del código del Método Simplex. El método fue probado con los ejemplos vistos en clase (los cuales se encuentran en el archivo problemas_de_prueba.m) y funcionó correctamente. Sin embargo, hace falta revisar el caso en el que el problema tiene un conjunto factible vacío.

---
+++ mSimplexFaseII.m
@@ -0,0 +1,157 @@
+function [x0, z0, ban, iter] = mSimplexFaseII(A, b, c) 
+
+% Esta función realiza la Fase II del Metodo Simplex para problemas
+% que tienen la siguiente forma
+%
+%               minimizar   c^T x 
+%               sujeto a    Ax <= b , x >= 0 , b >= 0 
+% 
+% In :  A ... mxn matrix 
+%       b ... column vector with as many rows as A 
+%       c ... column vector with as many entries as one row of A 
+% 
+% Out:  xo ..... SFB optima del problema 
+%       zo ..... valor optimo del problema 
+%       ban .... indica casos: 
+%           -1 ... si el conjunto factible es vacio 
+%           0 .... si se encontro una solucion optima 
+%           1 .... si la funcion objectivo no es acotada. 
+%       iter ... numero de iteraciones (cambios de variables basicas) 
+%       que hizo el metodo
+
+    % 1 Construccion del tableau en estado 0 
+    
+    format rat; % MATLAB imprime fracciones en vez de decimales
+
+    [m, n] = size(A); % m variables basicas
+    iter = 0;
+    ban = 0;
+    
+    % Solo imprimimos el tableau y los pasos para problemas
+    % pequeños
+    imprimirTableau = true;
+    if n + m > 15
+        imprimirTableau = false;
+    end
+
+    % 1.1 Calculamos los costos relativos iniciales 
+    % En estado 0, A_B = Identidad
+    c_N = c';
+    c_B = zeros(1, m);
+    c = [c_N c_B];
+    lambda = c_B;
+    r_N = lambda*A - c_N;
+
+    % 1.2 Construccion del tableau inicial y definicion 
+    % de los conjuntos B y N
+    N = 1:n;
+    B = n+1:m+n;
+    T = construirTableau(eye(m), A, c_B, r_N, b, B, N, iter, imprimirTableau);
+    h = T(1:m, m+n+1);
+    
+    % Si el conjunto factible es vacio, el Metodo Simplex no
+    % tendra opcion mas que escoger un punto que no cumpla
+    % la restriccion de no-negatividad
+    if any(h < 0)
+        fprintf("\nConjunto factible vacio\n");
+        ban = -1;
+    end
+    
+    % 2 Probamos la condicion de optimalidad
+    while any(r_N > 0) && ban == 0
+        
+        % 2.1 Seleccionamos la variable de entrada mediante
+        % la Regla de Bland
+        e = find(r_N > 0, 1);
+        
+        % 3 Probamos si el problema es acotado
+        if any(T(1:m, N(e)) > 0)
+            
+            % 3.1 Seleccion de la variable de salida mediante
+            % la Regla de Bland
+
+            % Buscamos los indices de los denominadores no-positivos
+            noPositivos = T(1:m, N(e)) <= 0; 
+
+            % Calculamos los cocientes y asignamos infinito a los
+            % cocientes que corresponden a los denominadores 
+            % menores o iguales a cero
+            h = T(1:m,m+n+1);
+            cocientes = h./T(1:m, N(e));
+            cocientes(noPositivos) = inf;
+
+            % s es el indice que corresponde al minimo de cocientes
+            [~, s] = min(cocientes);
+            
+            if imprimirTableau
+                fprintf("La variable de entrada es X%d y la variable de salida es X%d\n",N(e),B(s));
+            end
+            
+            % 4 Redefinimos los conjuntos B y N 
+            aux = B(s);
+            B(s) = N(e);
+            N(e) = aux;
+            iter = iter + 1;
+            
+            % Definimos las nuevas matrices y los nuevos costos
+            % asociados a nuestras variables basicas y no-basicas
+            AB = T(1:m, B);
+            AN = T(1:m, N);
+            c_B = c(B);
+            c_N = c(N);
+            
+            % 4.1 Calculamos los nuevos costos relativos y 
+            % construimos el nuevo tableau 
+            lambda = (AB')\c_B';
+            lambda = lambda';
+            r_N = lambda*AN - c_N;
+            T = construirTableau(AB, AN, c_B, r_N, h, B, N, iter, imprimirTableau);
+            
+        else
+            
+            % El problema es no-acotado. 
+            ban = 1;
+            break;
+        
+        end
+        
+    end
+    
+    x0 = AB\h;
+    z0 = c_B*x0;
+    x0 = x0(B <= n); % Solo devolvemos los valores de las variables originales
+    
+    return;
+  
+end
+
+function [T] = construirTableau(A_B, A_N, c_B, r_N, b, B, N, iter, imprimirTableau)
+
+% Esta funcion construye el tableau correspondiente a la base B
+% usando las matrices A_B, A_N y los coeficientes c_B y c_N
+
+    m = length(B);
+    n = length(N);
+
+    T = zeros(m+1,m+n+1);
+    
+    h = A_B\b;
+    H = A_B\A_N;
+    z = c_B*h;
+
+    T(1:m, B) = eye(m);
+    T(1:m, N) = H;
+    T(1:m, m+n+1) = h;
+    T(m+1, N) = r_N;
+    T(m+1, m+n+1) = z;
+    
+    if imprimirTableau
+        fprintf("\nIteracion %d\n\n", iter);
+        disp(T);
+        fprintf("Variables basicas: ");
+        disp(B);
+    end
+
+    return;
+
+end
---
+++ problemas_de_prueba.m
@@ -0,0 +1,57 @@
+
+% Problemas de prueba para la funcion mSimplexFaseII
+
+% Correr el metodo con [x0, z0, ban, iter] = mSimplexFaseII(A,b,c)
+
+% 1 Ejemplos de clase
+
+    % 1.1 Problema acotado con conjunto factible no vacio
+    
+    % Solucion: z* = -36, B = {1,2,3}, x* = (2,6,2)
+    A = [1 0; 0 2; 3 2]; b = [4; 12; 18]; c = [-3; -5];
+
+    % 1.2 Problema con SBF degenerada
+    
+    % Solucion: z* = -2, B = {1,2}, x* = (2,2)
+    % SBF degenerada en el estado 0
+    A = [-1 1; 1 0]; b = [0; 2]; c = [0; -1];  
+    
+    % 1.3 Problema no acotado
+    
+    % No acotado en la 1ra iteracion
+    A = [1 -1; -1 1]; b = [1; 2]; c = [-1; 0];
+    
+% 2 Vanderbei 
+
+    % 2.1 Problema acotado de maximizacion con conjunto
+    % factible no vacio
+    
+    % Solucion: z* = 17, B = {1,3}, x* = (2,0,1,0)
+    A = [2 1 1 3; 1 3 1 2]; b = [5; 3]; c = [6; 8; 5; 9];
+    
+    % 2.2 Problema de maximizacion con conjunto factible 
+    % vacio
+    
+    A = [-1 -1; -1 1; 1 2]; b = [-3; -1; 2]; c = [1; 3];
+    
+    % 2.3 Problema de maximizacion no acotado
+    
+    A = [-1 -1; -1 1; -1 2]; b = [-3; - 1; 2]; c = [1; 3];
+    
+    % 2.4 Problema con SBF degenerada
+    
+    % Solucion: x* = TBT
+    A = [1 0 4 1 -5 -2 3 -6; 
+         0 1 -3 -1 4 1 -2 5;
+         1 0 0 0 0 0 0 0;
+         0 1 0 0 0 0 0 0;
+         0 0 1 0 0 0 0 0;
+         0 0 0 1 0 0 0 0;
+         0 0 0 0 1 0 0 0;
+         0 0 0 0 0 1 0 0;
+         0 0 0 0 0 0 1 0;
+         0 0 0 0 0 0 0 1];
+     b = [7; 3; 8; 6; 10; 15; 2; 10; 4; 3];
+     c = [-3; -1; 1; 2; -1; 1; -1; -4];
+    
+    
